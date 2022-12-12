ad4fb7ddda8118d7e62b7467d7b8ffb95b9b4601
Viktor <jude.peper@gmail.com>
2022-12-12 22:18:15 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Component } from 'react';
 import { toast, Toaster } from 'react-hot-toast';
-import { ImageGallery } from './components/ImageGallery/ImageGallery';
-import { SearchBar } from './components/SearchBar/SearchBar';
-import { searchImages } from './components/Api/api';
-import { Button } from './components/Button/Button';
-import { Section } from 'components/Section/Section';
-import { Loader } from 'components/Loader/Loader';
+import { ImageGallery } from '../src/components/ImageGallery/ImageGallery';
+import { SearchBar } from '../src/components/SearchBar/SearchBar';
+import { searchImages } from '../src/components/Api/api';
+import { Button } from '../src/components/Button/Button';
+import { Section } from '../src/components/Section/Section';
+import { Loader } from '../src/components/Loader/Loader';
 
 export class App extends Component {
   state = {
