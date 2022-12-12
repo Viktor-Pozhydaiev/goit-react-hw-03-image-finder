c3fefefa09639e878e2d2f918cdae800d7cb8bc1
Viktor <jude.peper@gmail.com>
2022-12-12 22:15:16 +0100

    add files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
+import { App } from '../src/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import { App } from 'App';
-=======
-import { App } from 'components/App';
->>>>>>> 5040d04ed57639443d16a2af6147d9364698df42
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
